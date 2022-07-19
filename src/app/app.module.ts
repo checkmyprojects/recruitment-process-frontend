1c388b8f586e8fe99e0b9f674653b870263cf11f
UndefinedBehaviourAndIllFormed <sardevcpp@gmail.com>
2022-07-19 12:25:51 +0200

    fixed new candidate form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
