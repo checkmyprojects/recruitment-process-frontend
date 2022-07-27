ceb07df1493c67e6cd99c2f0da8b7160ea6f942a
checkmyprojects <notnow.plz+github@gmail.com>
2022-07-27 09:57:45 +0200

    test 1 function

--- src/app/admin/list/list.component.spec.ts
+++ src/app/admin/list/list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
@@ -8,7 +9,8 @@ describe('ListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
+      declarations: [ ListComponent ],
+      imports: [HttpClientTestingModule],
     })
     .compileComponents();
   });
@@ -16,10 +18,24 @@ describe('ListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
+    component.user = {
+      "id": 3,
+      "name": "people",
+      "username": "people",
+      "email": "people@mail.com",
+      "roles": ["admin"],
+      "active": true
+  }
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change isEdit to true/false when calling the function', () => {
+    component.isEdit = false;
+    component.isEditToggle();
+    expect(component.isEdit).toBe(true);
   });
 });
