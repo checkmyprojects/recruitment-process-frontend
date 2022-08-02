96e6266002a7b1d477ca2605e63d82e6a1419395
Lupe13 <lupe.flores.r@gmail.com>
2022-08-02 12:37:34 +0200

    list candidates test

--- src/app/candidates/list-candidates/list-candidates.component.spec.ts
+++ src/app/candidates/list-candidates/list-candidates.component.spec.ts
@@ -1,3 +1,5 @@
+import { CandidatesService } from './../../_services/candidates.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListCandidatesComponent } from './list-candidates.component';
@@ -5,12 +7,20 @@ import { ListCandidatesComponent } from './list-candidates.component';
 describe('ListCandidatesComponent', () => {
   let component: ListCandidatesComponent;
   let fixture: ComponentFixture<ListCandidatesComponent>;
+  let service: CandidatesService;
+  let httpMock: HttpTestingController;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListCandidatesComponent ]
+      declarations: [ ListCandidatesComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [ CandidatesService ]
     })
     .compileComponents();
+    httpMock = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(CandidatesService);
+
   });
 
   beforeEach(() => {
@@ -21,5 +31,28 @@ describe('ListCandidatesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getCandidates on ngOnInit with a GET method', () => {
+    const candidate: any[] = [
+      {
+          "id": 1,
+          "name": "Francisco",
+          "surname": "Domínguez",
+          "email": "frando@mail.com",
+          "skills": "Angular, Typescript, Java",
+          "studies": "F.P.",
+          "location": "Sevilla",
+          "experience": 1,
+          "hired": false,
+          "state": null,
+          "interviews": null
+      }
+    ]
+    const req = httpMock.expectOne('http://localhost:8080/api/candidate/list');
+    expect(req.request.method).toEqual("GET");
+    req.flush(candidate);
+    httpMock.verify();
+
   });
 });
