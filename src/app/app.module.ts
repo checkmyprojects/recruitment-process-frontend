8e8f8bd6997b1aa69c2130a88046ba4d1d162a1a
checkmyprojects <notnow.plz+github@gmail.com>
2022-08-21 12:43:18 +0200

    modal view/edit itnerview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { NewInterviewComponent } from './components/interview/new-interview/new-
 import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
 import { ModalSelectionComponent } from './components/selection/modal-selection/modal-selection.component';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
 import { ModalInterviewComponent } from './components/interview/modal-interview/modal-interview.component';
 @NgModule({
   declarations: [
@@ -105,6 +106,7 @@ import { ModalInterviewComponent } from './components/interview/modal-interview/
     NgxMatDatetimePickerModule,
     NgxMatNativeDateModule,
     NgxMatTimepickerModule,
+    MatTabsModule,
     ReactiveFormsModule,
     MatSnackBarModule
   ],
