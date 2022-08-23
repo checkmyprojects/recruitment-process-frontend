7f8ab2aef278e12cd19b62a96d46f538fe53a59b
devfdom <devfdom@gmail.com>
2022-08-23 13:33:26 +0200

    dashboard with bootstrap

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+// export interface Tile {
+//   color: string;
+//   cols: number;
+//   rows: number;
+//   text: string;
+// }
 
 /**
  * @title dinamic grid-list
@@ -16,11 +16,11 @@ export interface Tile {
   templateUrl: 'dashboard.component.html',
 })
 export class DashboardComponent {
-  tiles: Tile[] = [
-    { text: 'Administración', cols: 3, rows: 2, color: 'lightblue' },
-    { text: 'Candidatos', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Procesos', cols: 1, rows: 2, color: 'lightpink' },
-    { text: 'Entrevistas', cols: 2, rows: 2, color: '#DDBDF1' },
-    { text: 'Estadísticas', cols: 1, rows: 2, color: '#DDBDF1' },
-  ];
+  // tiles: Tile[] = [
+  //   { text: 'Administración', cols: 3, rows: 2, color: 'lightblue' },
+  //   { text: 'Candidatos', cols: 1, rows: 2, color: 'lightgreen' },
+  //   { text: 'Procesos', cols: 1, rows: 2, color: 'lightpink' },
+  //   { text: 'Entrevistas', cols: 2, rows: 2, color: '#DDBDF1' },
+  //   { text: 'Estadísticas', cols: 1, rows: 2, color: '#DDBDF1' },
+  // ];
 }
