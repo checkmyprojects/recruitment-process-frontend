403f25280cfa1b48db6bcf1249a90b64c2b6a121
devfdom <devfdom@gmail.com>
2022-08-24 10:37:10 +0200

    update icons

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,26 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
-// export interface Tile {
-//   color: string;
-//   cols: number;
-//   rows: number;
-//   text: string;
-// }
-
-/**
- * @title dinamic grid-list
- */
 @Component({
   selector: 'dashboard.component',
   styleUrls: ['dashboard.component.css'],
   templateUrl: 'dashboard.component.html',
 })
-export class DashboardComponent {
-  // tiles: Tile[] = [
-  //   { text: 'Administración', cols: 3, rows: 2, color: 'lightblue' },
-  //   { text: 'Candidatos', cols: 1, rows: 2, color: 'lightgreen' },
-  //   { text: 'Procesos', cols: 1, rows: 2, color: 'lightpink' },
-  //   { text: 'Entrevistas', cols: 2, rows: 2, color: '#DDBDF1' },
-  //   { text: 'Estadísticas', cols: 1, rows: 2, color: '#DDBDF1' },
-  // ];
+export class DashboardComponent implements OnInit {
+
+  roles: string[] = [];
+
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showCandidatesBoard = false;
+  showBusinessBoard = false;
+  showInterviewerBoard = false;
+  showSelectionBoard = false;
+  username?: string;
+  email?: string;
+
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      if(this.roles.includes('ROLE_ADMIN')){
+        this.showAdminBoard = true;
+      }if(this.roles.includes('ROLE_PEOPLE') || this.roles.includes('ROLE_ADMIN')){
+        this.showCandidatesBoard = true;
+        this.showInterviewerBoard = true;
+        this.showSelectionBoard = true;
+      }if(this.roles.includes('ROLE_BUSINESS') || this.roles.includes('ROLE_ADMIN')){
+        this.showBusinessBoard = true;
+      }if(this.roles.includes('ROLE_INTERVIEWER')){
+        this.showInterviewerBoard = true;
+      }
+
+      this.email = user.email;
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
