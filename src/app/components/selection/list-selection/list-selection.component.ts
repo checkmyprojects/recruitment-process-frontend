36cafa8f3bb5d3dc94bfed4ffbe9499813e426a0
Lupe13 <lupe.flores.r@gmail.com>
2022-08-26 09:18:31 +0200

    pdf selection

--- src/app/components/selection/list-selection/list-selection.component.ts
+++ src/app/components/selection/list-selection/list-selection.component.ts
@@ -9,6 +9,8 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalSelectionComponent } from '../modal-selection/modal-selection.component';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-list-selection',
@@ -155,4 +157,31 @@ export class ListSelectionComponent implements OnInit {
       }
     });
   }
+
+  Selection = [];
+  downloadPDF() {
+    const DATA = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+
+  }
+
 }
