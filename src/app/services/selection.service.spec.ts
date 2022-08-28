fc7911435ccc150c5303080d2cbecfc670a58fa5
Lupe13 <lupe.flores.r@gmail.com>
2022-08-28 14:15:56 +0200

    selection service test

--- src/app/services/selection.service.spec.ts
+++ src/app/services/selection.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { request } from 'http';
 
 import { SelectionService } from './selection.service';
 
@@ -7,7 +8,7 @@ import { SelectionService } from './selection.service';
 describe('SelectionService', () => {
   let service: SelectionService;
   let httpMock: HttpTestingController;
-  
+
   beforeEach(() => {
     TestBed.configureTestingModule({
 
@@ -35,4 +36,66 @@ describe('SelectionService', () => {
 
     httpMock.verify();
   });
+
+  it('updateSelections() and be a GET request and call the right URL', () =>{
+    const names: any= {name: 'a'};
+
+    service.updateSelections(names).subscribe((res) => {
+      expect(res).toEqual(names);
+    })
+
+    const req = httpMock.expectOne('http://localhost:8080/api/selection/edit');
+    expect(req.request.method).toEqual("PUT");
+    expect(req.request.url).toBe('http://localhost:8080/api/selection/edit');
+    req.flush(names);
+
+    httpMock.verify();
+  });
+
+  it('endSelectionById() should return correct selection', () => {
+    const id= 1;
+    let user = {id: 1, name: "name"}
+
+
+    service.endSelectionById(id).subscribe((res) => {
+      expect(res).toEqual(user);
+
+    });
+
+    const req = httpMock.expectOne(`http://localhost:8080/api/selection/end/${id}`);
+    expect(req.request.method).toEqual("GET");
+    expect(req.request.url).toBe(`http://localhost:8080/api/selection/end/${id}`);
+    req.flush(user);
+
+    httpMock.verify();
+  });
+
+  it ('deleteSelectionById() should be a DELETE request and call proper ID url', ()=> {
+
+    const selection: any = {"id": 1};
+
+    service.deleteSelectionById(selection.id).subscribe((res) => {
+  })
+
+  const req = httpMock.expectOne(`http://localhost:8080/api/selection/delete/${selection.id}`);
+  expect(req.request.method).toEqual('DELETE');
+  expect(req.request.url).toBe(`http://localhost:8080/api/selection/delete/${selection.id}`);
+
+  req.flush(selection.id);
+
+  httpMock.verify();
+});
+it ('registerNewSelection should be a post request', () => {
+
+  let newSelection: any = {name: 'a'}
+
+  service.registerNewSelection(newSelection).subscribe((res) => {
+    expect(res).toEqual(newSelection);
+  });
+
+  const req = httpMock.expectOne('http://localhost:8080/api/selection/new');
+  expect(req.request.method).toEqual("POST");
+  req.flush(newSelection);
+
+});
 });
